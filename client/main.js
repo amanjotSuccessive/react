6bae201477f91c0765f517fdac4a36fdd55a23a7
amanjotSucessive <amanjot.singh@successive.tech>
2019-05-15 18:29:59 +0530

    WIP

--- client/main.js
+++ client/main.js
@@ -4,13 +4,6 @@ import App from './../imports/ui/App'
 import { Tracker } from 'meteor/tracker';
 import {render}  from 'react-dom';
 
-/*
- setTimeout(() => {
-  dbPlayers = Players.find().fetch();
-  console.log('fff', dbPlayers)
-}, 1000); 
-*/
-
 Meteor.startup(() => {
 
   Tracker.autorun(() => {
